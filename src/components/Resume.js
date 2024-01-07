a8c3e77c6829c542e24aba45d8668151b15ff884
thegokulakannan <gokulakannanbgk@gmail.com>
2024-01-07 18:54:11 +0530

    final portfolio

---
+++ src/components/Resume.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import "./resume.css";
+
+
+export default  class Resume extends Component {
+  
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+
+         <div className="row education">
+
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.education && resumeData.education.map((item)=>{
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+         </div>
+        <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Work</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.work && resumeData.work.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.CompanyName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+
+
+         <div className="row skill">
+
+            <div className="col-md-3 header-col">
+               <h1><span>Skills</span></h1>
+            </div>
+
+            <div className="col-md-9 main-col text-center">
+
+            <div className="skills-container d-flex flex-wrap justify-content-center">
+            {resumeData.skills &&
+                resumeData.skills.map((item, index) => (
+                  <span key={index} className="skill-item">
+                   <strong> {item.skillname} |</strong>
+                  </span>
+                ))}
+            </div>
+
+   			 </div>
+
+         </div>
+
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/thegokulakannan",
+    "name": "Gokulakannan B",
+    "role": "Frontend Developer",
+    "PhoneNo":" +91-7904865367",
+    "emailId": " gokulakannanbgk@gmail.com",
+    "roleDescription": "I possess a diverse interest in various facets of frontend development, continually exploring and acquiring knowledge about emerging technologies. In my spare time, I enjoy not only delving into new technologies but also producing technical articles. Additionally, I find relaxation and enjoyment in gaming activities.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/gokulakannanb/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/thegokulakannan",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/thegokulakannan",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"As a Project Engineer at Wipro, I have been developing front end solutions for various clients using HTML, CSS, and JavaScript. I have contributed to  projects, such as creating a responsive web application and enhancing the user interface. I am passionate about staying ahead in tech and exploring modern frameworks, I am also eager to broaden my skills in TypeScript, a language that adds static typing to JavaScript and improves its scalability and reliability ",
+    "address":"India",
+    "website":"gokulakannanbgk@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Dr Mahalingam college of engineering and technology",
+        "specialization":"BE - Computer science and engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+
+      },
+      {
+        "UniversityName":"Nachimuthu polytechnic college",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"may",
+        "YearOfPassing":"2019",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Wipro",
+        "specialization":"Project engineer",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2022 to Present",
+        "Achievements":"Worked as a front end developer , contributed to the development of modern and responsive web applications using React.js, which included creating and maintaining various UI components and ensuring seamless data flow within the application.",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"BootStrap"
+      },
+      {
+        "skillname":"jest"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"git"
+      },
+      {
+        "skillname":"GitHub"
+      },
+      {
+        "skillname":"API testing"
+      },
+      {
+        "skillname":"Jira"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Simon-game",
+        "description":"interactive web app",
+        "imgurl":"images/portfolio/simongame.png",
+        "codeURL" : "https://thegokulakannan.github.io/simon-game.github.io/",
+      },
+      {
+        "name":"Blog",
+        "description":"Blog using express and ejs",
+        "imgurl":"images/portfolio/blog.png",
+        "codeURL":"https://github.com/thegokulakannan/Blog-API-project",
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
